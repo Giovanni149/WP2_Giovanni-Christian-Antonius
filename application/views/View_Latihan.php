6a2fd5607be10134ad413d4c05b076fdaefe243b
Giovanni149 <ivangiovanni149@gmail.com>
2022-09-25 11:20:53 +0700

    Menambahkan File

---
+++ application/views/View_Latihan.php
@@ -0,0 +1,15 @@
+<html>
+
+<head>
+    <title>Latihan 1</title>
+</head>
+
+<body>
+    Halo Kawan.. Mari Kita Belajar Web Programming..!<br>
+    Nilai 1 = <?= $nilai1; ?>
+    Nilai 2 = <?= $nilai2; ?>
+    Ini Hasil Dari Pemodelan Dengnan Metode Penjumlahan Yaitu <?=
+$nilai1 . " = " . $nilai2 . " = " . $hasil; ?>
+</body>
+
+</html>
